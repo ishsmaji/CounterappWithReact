65adc1382e7694d972c3afdfc959052c1d2b30a1
Isha <ishamaji002002@gmail.com>
2023-11-19 15:13:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  function decresehaldeler() {
+    setCount(count - 1);
+  }
+  function incresehaldeler() {
+    setCount(count + 1);
+  }
+  function resethaldeler() {
+    setCount(0);
+  }
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">
+        Increment & Decrement
+      </div>
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button
+          onClick={decresehaldeler}
+          className="border-r-2 text-center w-20 border-[#b1bfbf] text-5xl"
+        >
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">{count}</div>
+        <buttonn
+          onClick={incresehaldeler}
+          className="border-r-2 text-center w-20 border-[#b1bfbf] text-5xl"
+        >
+          +
+        </buttonn>
+      </div>
+      <button
+        onClick={resethaldeler}
+        className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"
+      >
+        reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
